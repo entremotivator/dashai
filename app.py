84dd37ee13aa800be66663985364f5aa3e368835
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-20 17:06:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,56 +27,56 @@ def main():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        if st.button("List Files and Folders"):
+        if st.button("List Files and Folders", key="list_files"):
             files_folders_df = list_files_folders()
             st.write("### Files and Folders:")
             st.write(files_folders_df)
 
-        if st.button("Upload New Themes"):
+        if st.button("Upload New Themes", key="upload_themes"):
             st.write("Functionality for uploading new themes.")
 
-        if st.button("Edit Theme Files"):
+        if st.button("Edit Theme Files", key="edit_theme_files"):
             st.write("Functionality for editing theme files.")
 
-        if st.button("Create Backup"):
+        if st.button("Create Backup", key="create_backup"):
             st.write("Functionality for creating a backup.")
 
-        if st.button("Organize Media Files"):
+        if st.button("Organize Media Files", key="organize_media"):
             st.write("Functionality for organizing media files.")
 
-        if st.button("Delete Unused Themes and Plugins"):
+        if st.button("Delete Unused Themes and Plugins", key="delete_unused"):
             st.write("Functionality for deleting unused themes and plugins.")
 
     with col2:
         uploaded_file = st.file_uploader("Upload a File", type=["zip", "tar.gz"])
         if uploaded_file is not None:
-            if st.button("Upload File"):
+            if st.button("Upload File", key="upload_file"):
                 upload_file(uploaded_file, os.path.join("uploads", uploaded_file.name))
                 st.success(f"File '{uploaded_file.name}' uploaded successfully.")
 
-        if st.button("Install and Update Plugins"):
+        if st.button("Install and Update Plugins", key="install_update_plugins"):
             st.write("Functionality for installing and updating plugins.")
 
-        if st.button("Customize CSS for Themes/Plugins"):
+        if st.button("Customize CSS for Themes/Plugins", key="customize_css"):
             st.write("Functionality for customizing CSS.")
 
-        if st.button("Delete Unused Themes and Plugins"):
+        if st.button("Delete Unused Themes and Plugins", key="delete_unused_plugins"):
             st.write("Functionality for deleting unused themes and plugins.")
 
     with col3:
-        if st.button("Download a File"):
+        if st.button("Download a File", key="download_file"):
             st.write("Functionality for downloading a file.")
 
-        if st.button("Update WordPress Core Manually"):
+        if st.button("Update WordPress Core Manually", key="update_wordpress"):
             st.write("Functionality for updating WordPress core manually.")
 
-        if st.button("Manage and Organize wp-content Folder"):
+        if st.button("Manage and Organize wp-content Folder", key="manage_organize"):
             st.write("Functionality for managing and organizing wp-content folder.")
 
-        if st.button("Upload Custom Fonts to Theme"):
+        if st.button("Upload Custom Fonts to Theme", key="upload_fonts"):
             st.write("Functionality for uploading custom fonts.")
 
-        if st.button("Search and Replace URLs in Database"):
+        if st.button("Search and Replace URLs in Database", key="search_replace_urls"):
             st.write("Functionality for searching and replacing URLs in the database.")
 
     # Additional Tabs and Buttons for Database Management, Performance Optimization, and Troubleshooting...
@@ -84,99 +84,99 @@ def main():
     col4, col5, col6 = st.columns(3)
 
     with col4:
-        if st.button("Export and Backup Database"):
+        if st.button("Export and Backup Database", key="export_backup_database"):
             st.write("Functionality for exporting and backing up the database.")
 
-        if st.button("Import Database Backup"):
+        if st.button("Import Database Backup", key="import_database_backup"):
             st.write("Functionality for importing a database backup.")
 
-        if st.button("Optimize and Repair Database Tables"):
+        if st.button("Optimize and Repair Database Tables", key="optimize_repair_tables"):
             st.write("Functionality for optimizing and repairing database tables.")
 
     with col5:
-        if st.button("Delete Unused Tables from Database"):
+        if st.button("Delete Unused Tables from Database", key="delete_unused_tables"):
             st.write("Functionality for deleting unused tables from the database.")
 
-        if st.button("Create New Database User"):
+        if st.button("Create New Database User", key="create_new_user"):
             st.write("Functionality for creating a new database user.")
 
-        if st.button("Modify User Permissions in Database"):
+        if st.button("Modify User Permissions in Database", key="modify_user_permissions"):
             st.write("Functionality for modifying user permissions in the database.")
 
     with col6:
-        if st.button("Execute SQL Queries"):
+        if st.button("Execute SQL Queries", key="execute_sql_queries"):
             st.write("Functionality for executing SQL queries.")
 
-        if st.button("Update Site URLs in Database"):
+        if st.button("Update Site URLs in Database", key="update_site_urls"):
             st.write("Functionality for updating site URLs in the database.")
 
     st.header("Performance Optimization")
     col7, col8, col9 = st.columns(3)
 
     with col7:
-        if st.button("Compress and Optimize Images"):
+        if st.button("Compress and Optimize Images", key="compress_optimize_images"):
             st.write("Functionality for compressing and optimizing images.")
 
-        if st.button("Minify CSS and JavaScript Files"):
+        if st.button("Minify CSS and JavaScript Files", key="minify_css_js"):
             st.write("Functionality for minifying CSS and JavaScript files.")
 
-        if st.button("Combine and Cache CSS/JS Files"):
+        if st.button("Combine and Cache CSS/JS Files", key="combine_cache_files"):
             st.write("Functionality for combining and caching CSS/JS files.")
 
     with col8:
-        if st.button("Implement Lazy Loading for Images"):
+        if st.button("Implement Lazy Loading for Images", key="lazy_loading_images"):
             st.write("Functionality for implementing lazy loading for images.")
 
-        if st.button("Enable Browser Caching via .htaccess"):
+        if st.button("Enable Browser Caching via .htaccess", key="enable_browser_caching"):
             st.write("Functionality for enabling browser caching via .htaccess.")
 
-        if st.button("Optimize and Compress Database Tables"):
+        if st.button("Optimize and Compress Database Tables", key="optimize_compress_tables"):
             st.write("Functionality for optimizing and compressing database tables.")
 
     with col9:
-        if st.button("Set up a Content Delivery Network (CDN)"):
+        if st.button("Set up a Content Delivery Network (CDN)", key="set_up_cdn"):
             st.write("Functionality for setting up a Content Delivery Network (CDN).")
 
-        if st.button("Clean up Old and Unnecessary Files"):
+        if st.button("Clean up Old and Unnecessary Files", key="clean_up_files"):
             st.write("Functionality for cleaning up old and unnecessary files.")
 
-        if st.button("Optimize .htaccess for Performance"):
+        if st.button("Optimize .htaccess for Performance", key="optimize_htaccess"):
             st.write("Functionality for optimizing .htaccess for performance.")
 
     st.header("Troubleshooting and Debugging")
     col10, col11, col12 = st.columns(3)
 
     with col10:
-        if st.button("Enable WordPress Debug Mode"):
+        if st.button("Enable WordPress Debug Mode", key="enable_debug_mode"):
             st.write("Functionality for enabling WordPress debug mode.")
 
-        if st.button("Review and Troubleshoot Error Logs"):
+        if st.button("Review and Troubleshoot Error Logs", key="review_troubleshoot_logs"):
             st.write("Functionality for reviewing and troubleshooting error logs.")
 
-        if st.button("Check and Fix Broken Links"):
+        if st.button("Check and Fix Broken Links", key="check_fix_broken_links"):
             st.write("Functionality for checking and fixing broken links.")
 
     with col11:
-        if st.button("Disable Problematic Plugins via FTP"):
+        if st.button("Disable Problematic Plugins via FTP", key="disable_plugins_ftp"):
             st.write("Functionality for disabling problematic plugins via FTP.")
 
-        if st.button("Update PHP Version for Your Website"):
+        if st.button("Update PHP Version for Your Website", key="update_php_version"):
             st.write("Functionality for updating the PHP version for your website.")
 
-        if st.button("Manually Reset WordPress Password"):
+        if st.button("Manually Reset WordPress Password", key="reset_wp_password"):
             st.write("Functionality for manually resetting WordPress password.")
 
     with col12:
-        if st.button("Restore Previous Version of a File"):
+        if st.button("Restore Previous Version of a File", key="restore_prev_version"):
             st.write("Functionality for restoring a previous version of a file.")
 
-        if st.button("Investigate and Fix White Screen of Death"):
+        if st.button("Investigate and Fix White Screen of Death", key="fix_white_screen"):
             st.write("Functionality for investigating and fixing the White Screen of Death.")
 
-        if st.button("Delete Transients from the Database"):
+        if st.button("Delete Transients from the Database", key="delete_transients"):
             st.write("Functionality for deleting transients from the database.")
 
-        if st.button("Disable WordPress Theme via FTP"):
+        if st.button("Disable WordPress Theme via FTP", key="disable_theme_ftp"):
             st.write("Functionality for disabling the WordPress theme via FTP.")
 
 if __name__ == '__main__':
